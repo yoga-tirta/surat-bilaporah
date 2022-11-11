e817a9f9fbbd46bcc69270a4c470f2020d9ef64c
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2022-11-11 20:40:17 +0700

    Update pengajuan_skk.php

--- tambah_pengajuan/pengajuan_skk.php
+++ tambah_pengajuan/pengajuan_skk.php
@@ -5,8 +5,9 @@ if(isset($_POST["button"])){
   $keperluan = $_POST["keperluan"];
   $nama = $_POST["nama"];
   $tempat_lahir = $_POST["tempat_lahir"];
+  $tgl = $_POST['waktu_pengajuan'];
   $tgl_lahir = $_POST["tgl_lahir"];
-  $gander = $_POST["gander"];
+  $gender = $_POST["gender"];
   $pekerjaan = $_POST["pekerjaan"];
   $alamat = $_POST["alamat"];
   $hari_meninggal = $_POST["hari_meninggal"];
@@ -14,11 +15,12 @@ if(isset($_POST["button"])){
   $nik_user = $_SESSION['nik'];
   $kode_surat = $_GET["kode_surat"];
 
+
   // set jam asia di jakarta
   date_default_timezone_set('Asia/Jakarta');
   $tgl = date('y-m-d H:i:s'); //mengambil jam dan tgl sekarang
 
-  $tb_pengajuan = mysqli_query($koneksi,"INSERT INTO tb_pengajuan(nik_user, kode_surat, waktu_pengajuan, keperluan, status_pengajuan) VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'Menunggu')");
+  $tb_pengajuan = mysqli_query($koneksi,"INSERT INTO tb_pengajuan(nik, kode_surat, tgl_pengajuan, keperluan, status_pengajuan) VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'Menunggu')");
   if($tb_pengajuan){
       header("location: ../tambah_data/tambah_skk.php?nama=$nama&tempat_lahir=$tempat_lahir&tgl_lahir=$tgl_lahir&gender=$gender&pekerjaan=$pekerjaan&alamat=$alamat&hari_meninggal=$hari_meninggal&tgl_meninggal=$tgl_meninggal&kode_surat=$kode_surat&tgl=$tgl");
   }
