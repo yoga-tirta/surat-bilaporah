ab46a8640105cd19bde42632159e9872a24bc4ba
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2022-11-11 20:41:17 +0700

    Update pengajuan_spa.php

--- tambah_pengajuan/pengajuan_spa.php
+++ tambah_pengajuan/pengajuan_spa.php
@@ -5,7 +5,7 @@ if(isset($_POST["button"])){
   $keperluan = $_POST["keperluan"];
   $nama_ayah = $_POST["nama_ayah"];
   $nama_ibu = $_POST["nama_ibu"];
-  $gander = $_POST["gander"];
+  $gender = $_POST["gender"];
   $agama = $_POST["agama"];
   $tempat_lahir = $_POST["tempat_lahir"];
   $tgl_lahir = $_POST["tgl_lahir"];
@@ -24,7 +24,8 @@ if(isset($_POST["button"])){
   date_default_timezone_set('Asia/Jakarta');
   $tgl = date('y-m-d H:i:s'); //mengambil jam dan tgl sekarang
 
-  $tb_pengajuan = mysqli_query($koneksi,"INSERT INTO tb_pengajuan(nik_user, kode_surat, waktu_pengajuan, keperluan, status_pengajuan) VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'Menunggu')");
+  $tb_pengajuan = mysqli_query($koneksi,"INSERT INTO tb_pengajuan(nik, kode_surat, tgl_pengajuan, keperluan, jenis_pengajuan, status_pengajuan) 
+                                                            VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'baru','Menunggu')");
   if($tb_pengajuan){
       header("location: ../tambah_data/tambah_spa.php?nama_ayah=$nama_ayah&nama_ibu=$nama_ibu&gender=$gender&agama=$agama&tempat_lahir=$tempat_lahir&tgl_lahir=$tgl_lahir&hubungan=$hubungan&pekerjaan=$pekerjaan&alamat=$alamat&nama_anak=$nama_anak&gender_anak=$gender_anak&tempat_lahir_anak=$tempat_lahir_anak&tgl_lahir_anak=$tgl_lahir_anak&waktu_lahir=$waktu_lahir&kode_surat=$kode_surat&tgl=$tgl");
   }
