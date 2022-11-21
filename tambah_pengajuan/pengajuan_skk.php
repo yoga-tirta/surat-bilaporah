e39f5eb4cabdf0c562a4d2c332c39ad470620f5e
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2022-11-22 00:05:47 +0700

    Update pengajuan_skk.php

--- tambah_pengajuan/pengajuan_skk.php
+++ tambah_pengajuan/pengajuan_skk.php
@@ -12,19 +12,19 @@ if(isset($_POST["button"])){
   $alamat = $_POST["alamat"];
   $hari_meninggal = $_POST["hari_meninggal"];
   $tgl_meninggal = $_POST["tgl_meninggal"];
+  $umur = $_POST["umur"];
+  $penyebab = $_POST["penyebab"];
   $nik_user = $_SESSION['nik'];
   $kode_surat = $_GET["kode_surat"];
+
 
   // set jam asia di jakarta
   date_default_timezone_set('Asia/Jakarta');
   $tgl = date('y-m-d H:i:s'); //mengambil jam dan tgl sekarang
 
-  $tb_pengajuan = mysqli_query($koneksi, "INSERT INTO tb_pengajuan(nik, kode_surat, tgl_pengajuan, keperluan, jenis_pengajuan, status_pengajuan) VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'Baru', 'Menunggu')");  
-  
+  $tb_pengajuan = mysqli_query($koneksi,"INSERT INTO tb_pengajuan(nik, kode_surat, tgl_pengajuan, keperluan, status_pengajuan) VALUES($nik_user,'$kode_surat','$tgl', '$keperluan', 'Menunggu')");
   if($tb_pengajuan){
-      header("location: ../tambah_data/tambah_skk.php?nama=$nama&tempat_lahir=$tempat_lahir&tgl_lahir=$tgl_lahir&gender=$gender&pekerjaan=$pekerjaan&alamat=$alamat&hari_meninggal=$hari_meninggal&tgl_meninggal=$tgl_meninggal&kode_surat=$kode_surat&tgl=$tgl");
-  } else {
-    echo 'Gagal';
+      header("location: ../tambah_data/tambah_skk.php?nama=$nama&tempat_lahir=$tempat_lahir&tgl_lahir=$tgl_lahir&gender=$gender&pekerjaan=$pekerjaan&alamat=$alamat&hari_meninggal=$hari_meninggal&tgl_meninggal=$tgl_meninggal&kode_surat=$kode_surat&tgl=$tgl&umur=$umur&penyebab=$penyebab");
   }
 }
 ?>
