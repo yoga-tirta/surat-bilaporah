0ebe2236c0083b6731b7a1cfd5f17f3ed5a9d504
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2022-11-22 00:05:12 +0700

    Update tambah_skk.php

--- tambah_data/tambah_skk.php
+++ tambah_data/tambah_skk.php
@@ -11,12 +11,14 @@ $hari_meninggal = $_GET["hari_meninggal"];
 $tgl_meninggal = $_GET["tgl_meninggal"];
 $nik_user = $_SESSION['nik'];
 $kode_surat = $_GET["kode_surat"];
+$umur = $_GET["umur"];
+$penyebab = $_GET["penyebab"];
 $tgl = $_GET["tgl"];
 
 $tb_pengajuan = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT tb_pengajuan.id_pengajuan FROM tb_pengajuan WHERE tb_pengajuan.nik='$nik_user' AND tb_pengajuan.tgl_pengajuan='$tgl'"));
 $id_pengajuan = $tb_pengajuan["id_pengajuan"];
-$tambah = mysqli_query($koneksi, "INSERT INTO tb_skk(id_pengajuan_skk, kode_surat, nama_skk, tempat_lahir_skk, gender_skk, pekerjaan_skk, alamat_skk, hari_meninggal_skk, tgl_meninggal_skk) 
-VALUES('$id_pengajuan', '$nik_user', '$kode_surat', '$tempat_lahir', '$gender', '$pekerjaan', '$alamat', '$hari_meninggal', '$tgl_meninggal')");
+$tambah = mysqli_query($koneksi, "INSERT INTO tb_skk(id_pengajuan_skk, kode_surat, nama_skk,tgl_lahir_skk, tempat_lahir_skk, gender_skk, pekerjaan_skk, alamat_skk, hari_meninggal_skk, tgl_meninggal_skk, umur_skk, penyebab_skk) 
+                                            VALUES('$id_pengajuan', '$kode_surat', '$nama', '$tgl_lahir','$tempat_lahir', '$gender', '$pekerjaan', '$alamat', '$hari_meninggal', '$tgl_meninggal', '$umur', '$penyebab')");
 
 if ($tambah) {
     $_SESSION["pesan"] = "sukses";
