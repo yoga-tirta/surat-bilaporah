44854aa34669d8c3ffa3bb5b55c4114a58919cc5
yoga-tirta <yogatirtapermana552@gmail.com>
2023-01-25 11:52:16 +0700

    update

--- admin/page_admin.php
+++ admin/page_admin.php
@@ -311,7 +311,7 @@ $nama = $tb_user["nama_user"];
                 </div>
             </div>
         </div>
-    </div>2
+    </div>
     <!-- Footer -->
     <footer class="sticky-footer bg-white">
         <div class="container my-auto">
